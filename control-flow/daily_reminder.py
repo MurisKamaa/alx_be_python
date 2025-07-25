55d4060b629225f0f93af8af6c00b97c6ed2bb58
MurisKamaa <engpaulkamau@gmail.com>
2025-07-25 15:42:09 +0300

    Created daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,27 +1,27 @@
-task = input("Describe the task: ")
-priority = input("What's the tasks priority (high, medium, low): ")
-time_bound = input("Is the task time bound (yes or no)? ")
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
     case "high":
         if time_bound == "yes":
-            print(f"Reminder: {task} is a high priority task that requires immediate attention!")
+            print(f"Reminder: '{task}' is a high priority task that requires immediate attention!")
         elif time_bound == "no":
-            print(f"Note: {task} is a high priority task that doesn't require immediate attention")
+            print(f"Note: '{task}' is a high priority task that doesn't require immediate attention")
         else:
             print("Input the correct time-bound condition.")
     case "medium":
         if time_bound == "yes":
-            print(f"Reminder: {task} is a medium priority task that requires immediate attention!")
+            print(f"Reminder: '{task}' is a medium priority task that requires immediate attention!")
         elif time_bound == "no":
-            print(f"Note: {task} is a medium priority task that doesn't require immediate attention")
+            print(f"Note: '{task}' is a medium priority task that doesn't require immediate attention")
         else:
                 print("Input the correct time-bound condition.")
     case "low":
         if time_bound == "yes":
-            print(f"Reminder: {task} is a low priority task that requires immediate attention!")
+            print(f"Reminder: '{task}' is a low priority task that requires immediate attention!")
         elif time_bound == "no":
-            print(f"Note: {task} is a low priority task that doesn't require immediate attention")
+            print(f"Note: '{task}' is a low priority task that doesn't require immediate attention")
         else:
             print("Input the correct time-bound condition.")
     case _:
