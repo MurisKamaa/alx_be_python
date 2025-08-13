718bba5beec4e84294bfe415b3c912738545140c
MurisKamaa <engpaulkamau@gmail.com>
2025-08-13 19:13:20 +0300

    Added No. of dp's in display_balance method

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,20 +1,20 @@
 class BankAccount:
     def __init__(self, account_balance):
-        self.account_balance = account_balance
+        self._account_balance = account_balance
 
     def deposit(self, amount):
         if amount <= 0:
             raise ValueError("Deposit amount must be positive.")
-        self.account_balance += amount
+        self._account_balance += amount
 
     def withdraw(self, amount):
         if amount <= 0:
             raise ValueError("Withdrawal amount must be positive.")
-        if amount <= self.account_balance:
-            self.account_balance -= amount
+        if amount <= self._account_balance:
+            self._account_balance -= amount
             return True
         else:
             return False
         
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}.")
+        print(f"Current Balance: ${self._account_balance:.2f}.")
